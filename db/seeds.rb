48c000e254d4656ab3aaa12b3f045a32e30072da
Beatriz Longley <beatrizsousacosta@gmail.com>
2020-08-13 18:52:11 +0200

    Create the controllers actions views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ing_serialized = open(url).read
+ing = JSON.parse(ing_serialized)
+
+ing['drinks'].each do |ingridient|
+  Ingredient.create(name: ingridient["strIngredient1"])
+end
+
+# Using the json url - you select the element you want to iterate over
+# ing = json parsing - calling the key inside the hash / object - ing['drinks']
+# Next step is to iterate over the key, to get inside the hashes inside the array
+# You could do it by saying ingridient["strIngredient1"]
+# Ingridient.create(name: ... ) - to create an instance of ingridient with the value
